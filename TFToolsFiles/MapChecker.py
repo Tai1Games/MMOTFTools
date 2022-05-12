ab43c24d7a2fd83b30b0b0185eee17e92ebfd12d
JuanmaSalva <47757003+JuanmaSalva@users.noreply.github.com>
2022-05-12 11:32:02 +0200

    Connecting nodes con codigo de error hecho

--- TFToolsFiles/MapChecker.py
+++ TFToolsFiles/MapChecker.py
@@ -1,17 +1,28 @@
 import json
 import io
+from Error import ERRCODE
+
+def checkConnectingNodes(roomsList, filePath, errorList, keyNames):
+	for room in roomsList:
+		for n in room['NodeConnections']:
+			name = room['NodeConnections'][n]['ConnectingNode']
+
+			if(keyNames.containsNode(name) != True):
+				errorList.append((ERRCODE.NODE_DOES_NOT_EXISTS, filePath,
+						  f"{name} node does not exists"))
+
+						  
 
 def checkAll(filesFolder, keyNames):
-    keyNames.print()
-    fails = list()
-    with io.open(filesFolder + '/mapejemplo.json', encoding='utf-8-sig') as json_data:
-        roomsList = json.loads(json_data.read())
-    
-    for room in roomsList:
-        for n in room['NodeConnections']:
-            name = room['NodeConnections'][n]['ConnectingNode']
+	errorList = list()
+	filePath = filesFolder + '/mapejemplo.json'
+	with io.open(filePath, encoding='utf-8-sig') as json_data:
+		roomsList = json.loads(json_data.read())
+	
 
-            if(keyNames.containsNode(name)):
-                print(f'{name} Lo tenemos')
-            else:
-                print(f'{name} No lo tenemos')
+	checkConnectingNodes(roomsList, filePath, errorList, keyNames)
+
+
+
+	for err in errorList:
+		print(err)

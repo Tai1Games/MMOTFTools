3bbbdc8c6c15cb98b48ddaac867215a1958242e4
Will7461 <43720307+Will7461@users.noreply.github.com>
2022-05-12 12:20:34 +0200

    check min attackas in enemy (#10)
    * check min attackas in enemy
    
    * Update EnemyChecker.py
    
    * Errors
    
    * Ya esta
    
    * import error
    
    Co-authored-by: Esteban <35503563+EstebenR@users.noreply.github.com>

--- TFToolsFiles/EnemyChecker.py
+++ TFToolsFiles/EnemyChecker.py
@@ -1,9 +1,10 @@
 import json
 import io
+from Error import ERRCODE, Error
 
-def statSize(filesFolder):
+def statSize(path):
 	fails = list()
-	with io.open(filesFolder +'/enemies.json', encoding='utf-8-sig') as json_data:
+	with io.open(path, encoding='utf-8-sig') as json_data:
 		enemyList = json.loads(json_data.read())
 	for item in enemyList:
 		try:
@@ -12,12 +13,43 @@ def statSize(filesFolder):
 		except KeyError:
 			try:
 				#Has no Stats property
-				fails.append(f'{item["Name"]} has no stat block')
+				fails.append(f'{item["Name"]} has no Stats block')
 			except KeyError:
 				#Doesn't event have a name lol
-				fails.append(f"An item is malformed!")
+				fails.append(f"An enemy is malformed!")
 
-	return fails
+	return len(fails), fails
+
+def attacksSize(path):
+	fails = list()
+	with io.open(path, encoding='utf-8-sig') as json_data:
+		enemyList = json.loads(json_data.read())
+
+	for item in enemyList:
+		try:
+			if len(item["Attacks"]) < 1:
+				fails.append(f'{item["Name"]} requires at least 1 attack')
+		except KeyError:
+			try:
+				#Has no Attacks property
+				fails.append(f'{item["Name"]} has no Attacks block')
+			except KeyError:
+				pass
+
+	return len(fails), fails
 
 def checkAll(filesFolder):
-	print(f"Stat size check errors: {len(statSize(filesFolder))}")
+	errorList = list()
+	filePath = filesFolder + '/enemies.json'
+	res, eMessages = statSize(filePath)
+	for err in eMessages:
+		errorList.append(Error(ERRCODE.ENEMY_STAT_SIZE, filePath, f"{err}"))
+	print(f"Stat size check errors: {res}")
+
+	res, eMessages = attacksSize(filePath)
+	for err in eMessages:
+		errorList.append(Error(ERRCODE.ENEMY_ATTACK_SIZE, filePath, f"{err}"))
+	print(f"Attacks size check errors: {res}")
+
+	for err in errorList:
+		print(err)

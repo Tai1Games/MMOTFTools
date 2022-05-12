7fec16e08e6a72274ede3b73546da553347fe73b
JuanmaSalva <47757003+JuanmaSalva@users.noreply.github.com>
2022-05-12 13:34:16 +0200

    Requisitos eventos

--- TFToolsFiles/MapChecker.py
+++ TFToolsFiles/MapChecker.py
@@ -1,6 +1,7 @@
 import json
 import io
 from Error import ERRCODE
+import Program
 
 def checkConnectingNodes(roomsList, filePath, errorList, keyNames):
 	for room in roomsList:
@@ -11,7 +12,25 @@ def checkConnectingNodes(roomsList, filePath, errorList, keyNames):
 				errorList.append((ERRCODE.NODE_DOES_NOT_EXISTS, filePath,
 						  f"{name} node does not exists"))
 
-						  
+
+def checkEventType(event, filePath, errorList, keyNames):
+	for e in event:
+		if(Program.checkEngineConstant(e['EventType'], "EVENTTYPE") != True):
+			errorList.append((ERRCODE.NODE_EVENT_DOES_NOT_EXIST, filePath,
+						f"{e['EventType']} event does not exist"))
+	return
+
+
+
+def checkEvents(roomsList, filePath, errorList, keyNames):
+	events = Program.getEngineConstants("EVENTS")
+	
+	for room in roomsList:
+		print("")
+		for event in events:			 
+			if(event in room):
+				checkEventType(room[event], filePath, errorList, keyNames)
+
 
 def checkAll(filesFolder, keyNames):
 	errorList = list()
@@ -21,6 +40,7 @@ def checkAll(filesFolder, keyNames):
 	
 
 	checkConnectingNodes(roomsList, filePath, errorList, keyNames)
+	checkEvents(roomsList, filePath, errorList, keyNames)
 
 
 

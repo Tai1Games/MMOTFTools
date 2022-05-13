30b6052da7883f8c54abf5c7cc57eaf888a64970
JuanmaSalva <47757003+JuanmaSalva@users.noreply.github.com>
2022-05-13 13:53:43 +0200

    Returns arreglados

--- TFToolsFiles/ItemChecker.py
+++ TFToolsFiles/ItemChecker.py
@@ -72,19 +72,9 @@ def checkAll(filesFolder, keyNames):
     with io.open(filePath, encoding='utf-8-sig') as json_data:
         itemList = json.loads(json_data.read())
 
-    # Repeat keys
-    RepeatKeysList = []
-    for item in itemList:
-        # TODO return for html
-        res, fails = Common.RepeatKeys("items.json", item)
-        if len(fails) > 0:
-            RepeatKeysList.append(fails)
-    print(f"{len(RepeatKeysList)} repeat key errors found.")
-
-
-    errorList = list()
+    errorList = []
     checkItemReferences(itemList, filesFolder, errorList, keyNames)
 
     print(f"{len(errorList)} items errors found.")
 
-    return fails
+    return errorList
